a60c17fcecb1e45e9506284aa03aedf0c81f30d9
Edwin Moquete <edwinmmoquete@gmail.com>
2020-10-28 18:59:24 -0400

    sdm660-common: vibrator: Add excluded-input-devices.xml
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -386,6 +386,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-ext

e68b85991e6813c177cafcffc81b2634677cb2ba
Mocarafee <harif.rp05@gmail.com>
2021-07-04 12:17:40 +0700

    sdm660-common: properties.mk: set proper vulkan name
    * As it in vendor/lib64/hw/vulkan.sdm660.so
    
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>
    Change-Id: I99aeac34ecef87b9f1479f31ec4b3374b4466570

--- properties.mk
+++ properties.mk
@@ -142,7 +142,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
     ro.hardware.egl=adreno \
-    ro.hardware.vulkan=adreno
+    ro.hardware.vulkan=sdm660
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \

c1511ce2cd2be5283c9bbda14d384aee83a589c7
Lunarixus <Nathan@lunarixus.party>
2021-09-04 00:11:59 +0530

    whyred: Add Pixel 2016 exclusive xml * This allows us to use unlimited backup in Google Photos infinitely.
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Change-Id: I461b6057baddb090ca94e838bca9d13a76d15fc8
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -219,6 +219,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_ODM_PROPERTIES += \
     ro.vendor.qti.va_odm.support=1
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # GPS / Location
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti \

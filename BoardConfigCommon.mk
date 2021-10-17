a8b78fce590eb44783cde1663dc3f895baac4866
TheSanty <sudhiryadav.igi@gmail.com>
2021-10-17 23:12:20 +0530

    sdm660-common: Set to permissive for now
    Change-Id: I26b20b6ed7908c5bbd62d9d8625085e2e2819138

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -33,6 +33,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb

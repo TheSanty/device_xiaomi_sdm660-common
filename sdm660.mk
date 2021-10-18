bb0bcb62754fdd4e719bfb1ad26e7d1e5366e63d
Kyle Harrison <khwebmail@gmail.com>
2021-10-18 15:05:02 +0100

    sdm660-common: props: Don't write binary XML files
    - Android 12 now writes XML files in binary format by default. This can cause incompatibility with TWRP which can hang when attempting to read XML files e.g. /data/system/storage.xml
    
    - This commit sets the persist.sys.binary_xml property to false so that XML files are written in text format.
    
    Change-Id: If6430597d8459e62dd78aa458c58237c71618970
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -396,6 +396,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
+# System
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
